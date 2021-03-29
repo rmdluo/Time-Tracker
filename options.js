2bd0d105cb3218dd707e306a973bc6044c562540
rmdluo <40529960+rmdluo@users.noreply.github.com>
2021-03-29 15:42:12 -0400

    Update options.js
    change for updated options page

--- options.js
+++ options.js
@@ -1,12 +1,20 @@
 var blockedSites = document.getElementById("blockedSites");
 
-chrome.storage.sync.get("settings", function(data){
-	this.settings = data.settings;
-	if(this.settings != null){
-		blockedSites.value = this.settings;
-	}
-});
+function getSettings() {
+	chrome.storage.sync.get("settings", function(data){
+		this.settings = data.settings;
+		if(this.settings != null){
+			blockedSites.value = this.settings;
+		}
+	});
+}
+
+getSettings();
 
 document.getElementById("save").onclick = function() {
 	chrome.storage.sync.set({"settings": blockedSites.value});
+};
+
+document.getElementById("cancel").onclick = function() {
+	getSettings();
 };
